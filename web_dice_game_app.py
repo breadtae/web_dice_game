d6f3c3add231fd04b8bec2520a2549677efe96ee
tae <breadtae0@gmail.com>
2023-11-13 00:18:09 +0900

    Created index and game_play page

--- web_dice_game_app.py
+++ web_dice_game_app.py
@@ -1,1 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask import jsonify
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    return render_template('index.html')
+
+@app.route('/start_game', methods=['GET', 'POST'])
+def start_game():
+
+    return render_template('play_game.html')
+
+
+if __name__ == '__main__':
+    print(f"Running {__name__} server")
+    app.run()
