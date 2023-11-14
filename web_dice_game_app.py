8171837d1f7e15a1f17d875bfc92c14685ac0e05
tae <breadtae0@gmail.com>
2023-11-14 20:29:37 +0900

    Enabled session to store user-unique values (play_num, dice_sum, dice_num)

--- web_dice_game_app.py
+++ web_dice_game_app.py
@@ -11,6 +11,10 @@ app.secret_key = "AA"
 app.debug = True
 
 
+def reset_session():
+    session.clear()
+    print("[Clear Session]")
+
 # Home page to show Game Entry
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -20,39 +24,73 @@ def home():
     3. If 'play' button is pressed, redirect to play_game.html
     :return:
     """
-    flash("Content for flash message")
+    reset_session()
+    flash(f"[Session] {session}")
 
     if request.method == 'POST':
-        play_num = request.form.get('play_num')
-        print(f"[RECEIVED] Play_num : {play_num}")
-        return redirect(url_for('start_game', play_num=play_num))
+        if 'play_game' in request.form:  # Select play_num from web
+            play_num = request.form.get('play_num')
+            session['play_num'] = play_num
+            print(f"[RECEIVED] Play_num : {play_num}")
+            return redirect(url_for('start_game', play_num=play_num))
 
     return render_template('index.html')
 
 
 @app.route('/start_game/<play_num>', methods=['GET', 'POST'])
 def start_game(play_num):
+    flash(f"[Session] {session}")
     print("redirected to /start_game")
 
     dice_num = []
+
     play_num = int(play_num)
     for num in range(play_num):
-        dice_num.append(random.randint(1, 6))
-
+        rand_num = random.randint(1, 6)
+        dice_num.append(rand_num)
+    # Sum randomly generated numbers
     dice_sum = sum(dice_num)
 
+    session['dice_num'] = dice_num
+    session['dice_sum'] = dice_sum
+
     if request.method == 'POST':
-        # TODO Enable submit and redirect
-        if request.form.get('submit', 'none') == 'submit':
-            print(f"[RECEIVED] return")
-             # Clear up session
-            return redirect(url_for('home'))
-        else:
-            print(f"RECEIVED {request.form}")
+        if 'submit_name' in request.form:
+            u_name = request.form.get('user_name', None)
+            print(f"[USER NAME] {u_name}")
+
+            # TODO Write data in Database
+            if not u_name:
+                print("User name is empty !")
+                return redirect()
+            else:
+                return redirect(url_for('score_board', user_name=u_name))
 
     return render_template('play_game.html', plays=play_num, dice_num=dice_num, sum=dice_sum)
 
 
+# TODO
+def write_db(user_name:str, score:int):
+    pass
+
+
+@app.route('/score_board/<user_name>', methods=['GET'])
+def score_board(user_name=None):
+    """
+    Shows score board of players
+    - Highlights user_name when render_template
+    - Reset every 30 min
+
+    Render Table from json database
+
+    :param user_name: Name received from user in play_game.html
+    :return:
+    """
+    flash(f"[Session] {session}")
+    print(f"redirected to /score_board/{user_name}")
+    return render_template('score_board.html', user_name=user_name)
+
+
 if __name__ == '__main__':
     print(f"Running {__name__} server")
-    app.run()
+    app.run(host='0.0.0.0', port=50003)
